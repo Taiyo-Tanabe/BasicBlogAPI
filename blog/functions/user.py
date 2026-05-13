c6a124256a4c7b25ba5db45c8f8a209e52a70a4b
Taiyo-Tanabe <taiyo.t.222@gmail.com>
2026-05-13 11:26:13 +0900

    fix user email

--- blog/functions/user.py
+++ blog/functions/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from ..hashing import Hash
 
 def create(request: User, db: Session):
-    new_user = models.User(name=request.name, email=request.name, password=Hash.bcrypt(request.password))
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
